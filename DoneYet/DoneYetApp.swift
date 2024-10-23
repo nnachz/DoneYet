8d6a5ef71f21a99ad53188f2bd900c9e30035da6
Kerem Türközü <turkozukerem@gmail.com>
2024-10-23 15:06:44 +0300

    Initial Commit

---
+++ DoneYet/DoneYetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoneYetApp.swift
+//  DoneYet
+//
+//  Created by Kerem Türközü on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoneYetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
